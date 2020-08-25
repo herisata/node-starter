6f2226358d4083cce14a76404231c98daede280b
herisata <heryratovoarison@gmail.com>
2020-08-25 17:05:18 +0300

    Express on docker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = 8000;
+const PORT = 4000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
